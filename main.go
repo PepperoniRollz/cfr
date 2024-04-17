94271226b65147fa560308f9d436bb502630da0a
pepperonirollz <cfitzpatrick616@gmail.com>
2024-04-17 00:22:06 -0400

    cleanup some comments, debugging printlns

--- main.go
+++ main.go
@@ -20,15 +20,14 @@ type Action int
 const (
 	Pass Action = iota
 	Bet
-	Check
-	Fold
 )
 
 const (
 	FirstAction GameState = iota
-	AiTurn
 	SecondAction
-	Resolving
+	PlayerFolded
+	AiFolded
+	Showdown
 )
 
 type Game struct {
@@ -42,11 +41,10 @@ type Game struct {
 	AiStrategy    []float64
 	Ai            kuhnTrainer
 	GameState     GameState
-	HandNumber    int
 	Pot           int
 	GameLog       string
 	ActionHistory string
-	StateChan     chan GameState
+	HandNumber    int
 }
 
 func newGame() *Game {
@@ -62,7 +60,7 @@ func newGame() *Game {
 		Ai:          trainer,
 		Pot:         0,
 		GameLog:     "Starting Game\n",
-		StateChan:   make(chan GameState, 1), // Buffered channel
+		GameState:   FirstAction,
 	}
 }
 
@@ -80,16 +78,15 @@ func newTemplate() *Templates {
 	}
 }
 func (g *Game) beginRound() {
-	if g.Running {
-		shuffle(g.Deck)
-		g.PlayerCard = g.Deck[0]
-		g.AiCard = g.Deck[1]
-		g.PlayerStack--
-		g.AiStack--
-		g.Pot = 2
-		g.GameLog = g.GameLog + fmt.Sprintf("Player 1 antes 1\nAi antes 1\nYou've been dealt a %d\n", g.PlayerCard) + "...waiting for action...\n"
-		g.GameState = FirstAction
-	}
+	g.GameState = FirstAction
+	shuffle(g.Deck)
+	g.PlayerCard = g.Deck[0]
+	g.AiCard = g.Deck[1]
+	g.PlayerStack--
+	g.AiStack--
+	g.Pot = 2
+	g.ActionHistory = ""
+	g.GameLog = g.GameLog + fmt.Sprintf("Player 1 antes 1\nAi antes 1\nYou've been dealt a %d\n", g.PlayerCard) + "...waiting for action...\n"
 }
 func (g *Game) aiAction() Action {
 	node := g.Ai.nodeMap[fmt.Sprintf("1 %d%s", g.AiCard, g.ActionHistory)]
@@ -104,7 +101,6 @@ func (g *Game) aiAction() Action {
 			break
 		}
 	}
-	fmt.Println("Ai strategy: ", action)
 	if action == 0 {
 		return Pass
 	} else {
@@ -112,85 +108,88 @@ func (g *Game) aiAction() Action {
 	}
 
 }
-func (g *Game) isTerminalState() bool {
-	length := len(g.ActionHistory)
-	if length > 1 {
-		if g.ActionHistory[length-1] == 'p' || g.ActionHistory[length-2:length] == "bb" {
-			return true
-		}
-	}
-	return false
-}
 
 func (g *Game) check() {
-
-	g.Lock()
+	fmt.Println("gamestate check", g.GameState)
 	g.ActionHistory = g.ActionHistory + "p"
 
 	if g.GameState == FirstAction {
-		g.GameState = AiTurn
 		g.GameLog = g.GameLog + "You have checked\n"
 		aiAction := g.aiAction()
 		if aiAction == Pass {
 			g.ActionHistory = g.ActionHistory + "p"
-			g.GameLog = g.GameLog + "The ai has checked\n"
-			//resolve hand
+			g.GameLog = g.GameLog + "The ai has checked behind\n"
+			g.GameState = Showdown
+			resolveRound(g)
 		} else {
 			g.ActionHistory = g.ActionHistory + "b"
 			g.GameLog = g.GameLog + "The ai has bet 1 unit\n"
 
 			g.Pot++
 			g.AiStack--
+			g.GameState = SecondAction
 		}
-		g.GameState = SecondAction
 	} else if g.GameState == SecondAction {
-		//resolve hand ai wins becasue you folded
-		g.GameState = Resolving
+		g.GameLog = g.GameLog + "You have folded\n"
+		g.GameState = PlayerFolded
+		resolveRound(g)
 	}
-	g.StateChan <- g.GameState // Send new state to channel
-	g.Unlock()
 }
 func (g *Game) bet() {
-	g.Lock()
 	g.ActionHistory = g.ActionHistory + "b"
 
 	if g.GameState == FirstAction {
-		g.GameState = AiTurn
 		g.GameLog = g.GameLog + "You have bet 1 unit\n"
 		g.PlayerStack--
 		g.Pot++
-		g.GameState = AiTurn
 		aiAction := g.aiAction()
 		if aiAction == Pass {
 			g.GameLog = g.GameLog + "The ai has folded\n"
-			//resolve hand becasue ai folded to bet
+			g.GameState = AiFolded
+			resolveRound(g)
+
 		} else {
 			g.Pot++
 			g.AiStack--
 			g.GameLog = g.GameLog + "The ai has called\n"
-			//resolve hand because ai has called a bet
+			g.GameState = Showdown
+			resolveRound(g)
 		}
-		g.GameState = Resolving
 	} else if g.GameState == SecondAction {
-		//you called
 		g.GameLog = g.GameLog + "You have called for 1 unit\n"
 		g.PlayerStack--
 		g.Pot++
-		//resolve hand
-		g.GameState = Resolving
+		g.GameState = Showdown
+		resolveRound(game)
 	}
-	g.StateChan <- g.GameState // Send new state to channel
-	g.Unlock()
 
 }
 
-func watchGameState(game *Game) {
-	for state := range game.StateChan { // Receive from channel
-		if state == Resolving {
-			fmt.Println("Game is now resolving...")
+func resolveRound(game *Game) {
 
-			// Handle the resolving state
+	state := game.GameState
+	if state == Showdown || state == PlayerFolded || state == AiFolded {
+		fmt.Println("Game is now resolving...")
+		switch state {
+		case Showdown:
+			game.GameLog = game.GameLog + fmt.Sprintf("You showdown a %d\nRobot shows down a %d\n", game.PlayerCard, game.AiCard)
+			if game.PlayerCard > game.AiCard {
+				game.PlayerStack += game.Pot
+				game.GameLog = game.GameLog + "You have won!\n"
+			} else {
+				game.AiStack += game.Pot
+				game.GameLog = game.GameLog + "Robot has won!\n"
+			}
+		case PlayerFolded:
+			game.AiStack += game.Pot
+			game.GameLog = game.GameLog + "Robot has won!\n"
+		case AiFolded:
+			game.PlayerStack += game.Pot
+			game.GameLog = game.GameLog + "You have won!\n"
 		}
+		game.Pot = 0
+		game.HandNumber++
+		game.beginRound()
 	}
 }
 
@@ -206,13 +205,11 @@ func main() {
 
 	e.POST("/start", func(c echo.Context) error {
 		game = newGame()
-		go watchGameState(game)
 		game.beginRound()
 		return c.Render(200, "dashboard", game)
 	})
 	e.POST("/pass", func(c echo.Context) error {
 		game.check()
-
 		return c.Render(200, "dashboard", game)
 	})
 	e.POST("/bet", func(c echo.Context) error {
